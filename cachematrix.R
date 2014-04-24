ada3a0b4167daace4c805354c21426a5d5bef932
zxmst30 <zxmst30@users.noreply.github.com>
2014-04-23 21:56:37 -0400

    Update cachematrix.R
    Added example output

--- cachematrix.R
+++ cachematrix.R
@@ -14,22 +14,40 @@ makeCacheMatrix <- function(x = matrix()) {
         setInverse <- function(solve) Inv <<- solve
         getInverse <- function() Inv  
         list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
-}  
+}
 
 
-## The cacheSolve function checks if the inverse of the matrix has already been 
-## saved in the cache and if yes, it returns the saved value. If the cache is empty, 
+## The cacheSolve function checks if the inverse of the matrix has already been
+## saved in the cache and if yes, it returns the saved value. If the cache is empty,
 ## the function calculates the inverse of the matrix using the solve function and returns it.
 
 cacheSolve <- function(x, ...) {
-        Inv <- x$getInverse()   #loads the cache from makeCacheMatrix into an object called Inv
+        Inv <- x$getInverse()  #loads the cache from makeCacheMatrix into an object called Inv
         if(!is.null(Inv)) {
                 message("getting cached data")
-                return(Inv)     #if cache is not empty, return the saved values
+                return(Inv)    #if cache is not empty, return the saved values
         }
-        data <- x$get()         # loads the matrix into an object called data
-        Inv <- solve(data)      # calculates the inverse of the matrix using the solve function
-        x$setInvers(Inv)        # saves the calculated inverse matrix to the cache
-        Inv                     # returns the inverse matrix
-}        
+        data <- x$get()        # loads the matrix into an object called data
+        Inv <- solve(data)     # calculates the inverse of the matrix using the solve function
+        x$setInvers(Inv)       # saves the calculated inverse matrix to the cache
+        Inv                    # returns the inverse matrix
+}
 
+
+# Sample output from these functions:
+> M1 <- makeCacheMatrix(matrix(1:4,2))
+> cacheSolve(M1)    # inverse matrix is calculated
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> cacheSolve(M1) 
+getting cached data # when solving the same matrix again, result is retrieved from cache
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> M2 <- makeCacheMatrix(matrix(5:8,2))
+> cacheSolve(M2)    # the inverse of a new matrix is calculated
+[,1] [,2]
+[1,]   -4  3.5
+[2,]    3 -2.5
+> 
